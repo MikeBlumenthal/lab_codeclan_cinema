31c754413be4a1fc1a8f5754711334de547a5417
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-17 11:07:26 +0100

    sets up directories and files

---
+++ specs/film_spec.rb
@@ -0,0 +1,8 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/customer')
+require_relative('../models/film')
+require_relative('../models/ticket')
+
+class TestFilm < Minitest::Test
+end
---
+++ specs/ticket_spec.rb
@@ -0,0 +1,8 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/customer')
+require_relative('../models/film')
+require_relative('../models/ticket')
+
+class TestTicket < Minitest::Test
+end
