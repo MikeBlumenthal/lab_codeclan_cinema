a45e76b13d0861bb2ff1f3c0fe4ece1795e5c115
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-17 14:10:54 +0100

    adds initialize method for ticket class

--- models/ticket.rb
+++ models/ticket.rb
@@ -2,4 +2,14 @@ require_relative('../db/sql_runner')
 
 class Ticket
 
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @customer_id = info['customer_id'].to_i
+    @film_id = info['film_id'].to_i
+  end
+
+
 end
