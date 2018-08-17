0eccb9e3275de834cf81deff2fd0bbb65826b4db
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-17 14:08:01 +0100

    adds initialize method for customer class

--- models/customer.rb
+++ models/customer.rb
@@ -2,4 +2,14 @@ require_relative('../db/sql_runner')
 
 class Customer
 
+  attr_reader :id
+  attr_accessor :
+
+  def initialize(info)
+    @id = info['id'].to_i if info['id']
+    @name = info['name']
+    @funds = info['funds'].to_i
+  end
+
+
 end
