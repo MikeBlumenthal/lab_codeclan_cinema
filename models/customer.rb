bf8e982ca55f37a79195222a31766e867e321d9a
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-17 14:50:52 +0100

    adds update method for customer

--- models/customer.rb
+++ models/customer.rb
@@ -23,6 +23,14 @@ class Customer
     @id = customer['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE customers
+    SET ( name, funds ) = ( $1, $2 )
+    WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run( sql, values )
+  end
+
   def Customer.all()
     sql = "SELECT * FROM customers"
     customers = SqlRunner.run( sql )
