49c98ded2b346e641a345bcade742996833aea1f
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-17 14:05:36 +0100

    fixes sql table creation

--- db/cc_cinema.sql
+++ db/cc_cinema.sql
@@ -16,6 +16,6 @@ CREATE TABLE films (
 
 CREATE TABLE tickets (
   id SERIAL8 PRIMARY KEY,
-  customer_id INT8 REFERENCES customers(id),
-  movie_id INT8 REFERENCES movies(id)
+  customer_id INT8 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE
 )
