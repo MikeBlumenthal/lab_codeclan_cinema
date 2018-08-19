f5ae530c1dd7a5486536a5f634c343d4b34cf291
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-19 21:37:36 +0100

    adds buy ticket method for customer and test, passes test

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -5,4 +5,25 @@ require_relative('../models/film')
 require_relative('../models/ticket')
 
 class TestCustomer < Minitest::Test
+
+  def setup
+    @customer1 = Customer.new({
+      'id' => '1',
+      'name' => 'Danny',
+      'funds' => '50'
+      })
+
+    @film1 = Film.new({
+      'id' => '1',
+      'title' => 'Face/Off',
+      'price' => '10'
+      })
+  end
+
+  def test_buy_ticket()
+    @customer1.buy_ticket(@film1)
+    actual = @customer1.funds
+    assert_equal(40, actual)
+  end
+
 end
--- specs/ticket_spec.rb
+++
@@ -1,8 +0,0 @@
-require('minitest/autorun')
-require('minitest/rg')
-require_relative('../models/customer')
-require_relative('../models/film')
-require_relative('../models/ticket')
-
-class TestTicket < Minitest::Test
-end
